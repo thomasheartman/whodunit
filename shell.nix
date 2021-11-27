73da17e6edc462a1b071d869bcf132346d2e44b5
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-11-27 11:37:25 +0100

    Project setup.

---
+++ shell.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+
+  fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") {};
+
+in
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    (
+      with fenix;
+      combine (
+        with default; [
+          cargo
+          clippy-preview
+          latest.rust-src
+          rust-analyzer
+          rust-std
+          rustc
+          rustfmt-preview
+        ]
+      )
+    )
+    cargo-edit
+    cargo-watch
+  ];
+}
