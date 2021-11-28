d278bba3cc5f07b389972de7a832fda49594b12e
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-11-28 19:58:06 +0100

    Add bench input files and update benchmarking.

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,11 +1,51 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use whodunit::algorithm;
 
 pub fn bench_algorithm(c: &mut Criterion) {
-    let input = std::fs::read_to_string("./input.json").unwrap();
+    let x10 = std::fs::read_to_string("./10.json").unwrap();
+    let x100 = std::fs::read_to_string("./100.json").unwrap();
+    let x1_000 = std::fs::read_to_string("./1000.json").unwrap();
+    let x10_000 = std::fs::read_to_string("./10_000.json").unwrap();
+    let x100_000 = std::fs::read_to_string("./100_000.json").unwrap();
 
-    c.bench_function("algo input", |b| b.iter(|| algorithm(&input)));
+    let mut group = c.benchmark_group("Algo range");
+
+    for (i, f) in [x10, x100, x1_000, x10_000, x100_000].iter().enumerate() {
+        let zeroes = "0".repeat(i + 1);
+        let id = format!("1{zeroes} elements");
+        group.bench_with_input(BenchmarkId::from_parameter(id), f, |b, input| {
+            b.iter(|| algorithm(input));
+        });
+    }
+
+    group.finish();
 }
 
-criterion_group!(benches, bench_algorithm);
+pub fn bench_algorithm_with_duplicates(c: &mut Criterion) {
+    // let x10 = std::fs::read_to_string("./10.json").unwrap();
+    // let x100 = std::fs::read_to_string("./100.json").unwrap();
+    // let x1_000 = std::fs::read_to_string("./1000.json").unwrap();
+    // let x10_000 = std::fs::read_to_string("./10_000.json").unwrap();
+    let x200_000 = std::fs::read_to_string("./200_000.json").unwrap();
+    // let x1_000_000 = std::fs::read_to_string("./1_000_000.json").unwrap();
+    let mut group = c.benchmark_group("Algo range with dupes");
+    for (i, f) in [
+        x200_000,
+        // x10, x100, x1_000, x10_000, x100_000,
+        // x1_000_000
+    ]
+    .iter()
+    .enumerate()
+    {
+        let zeroes = "0".repeat(i + 5);
+        let id = format!("2{zeroes} elements (duplicates >= 50%)");
+        // let duplicated = f.repeat(2);
+        group.bench_with_input(BenchmarkId::from_parameter(id), f, |b, input| {
+            b.iter(|| algorithm(input));
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_algorithm, bench_algorithm_with_duplicates);
 criterion_main!(benches);
