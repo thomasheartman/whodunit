de32ccd2bb3c1e8719b024699cd0499d48c082a5
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-12-02 17:43:43 +0100

    improve benching.

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,34 +1,17 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use whodunit::algorithm;
 
-pub fn bench_algorithm_max_997_unique(c: &mut Criterion) {
+pub fn bench_algorithm_unique_inputs(c: &mut Criterion) {
     let x10 = std::fs::read_to_string("./10.json").unwrap();
     let x100 = std::fs::read_to_string("./100.json").unwrap();
     let x1_000 = std::fs::read_to_string("./1000.json").unwrap();
-    let x10_000 = std::fs::read_to_string("./10_000.json").unwrap();
-    let x100_000 = std::fs::read_to_string("./100_000.json").unwrap();
-
-    let mut group = c.benchmark_group("Algo range");
-
-    for (i, f) in [x10, x100, x1_000, x10_000, x100_000].iter().enumerate() {
-        let zeroes = "0".repeat(i + 1);
-        let id = format!("1{zeroes} elements (max 997 unique)");
-        group.bench_with_input(BenchmarkId::from_parameter(id), f, |b, input| {
-            b.iter(|| algorithm(input));
-        });
-    }
-
-    group.finish();
-}
-
-pub fn bench_algorithm_all_unique(c: &mut Criterion) {
     let x10_000 = std::fs::read_to_string("./10_000-unique.json").unwrap();
 
     let mut group = c.benchmark_group("Unique inputs");
 
-    for (i, f) in [x10_000].iter().enumerate() {
-        let zeroes = "0".repeat(i);
-        let id = format!("10000{zeroes} unique elements");
+    for (i, f) in [x10, x100, x1_000, x10_000].iter().enumerate() {
+        let zeroes = "0".repeat(i + 1);
+        let id = format!("1{zeroes} unique elements");
         group.bench_with_input(BenchmarkId::from_parameter(id), f, |b, input| {
             b.iter(|| algorithm(input));
         });
@@ -50,8 +33,7 @@ pub fn bench_algorithm_with_duplicates(c: &mut Criterion) {
 
 criterion_group!(
     benches,
-    bench_algorithm_max_997_unique,
+    bench_algorithm_unique_inputs,
     bench_algorithm_with_duplicates,
-    bench_algorithm_all_unique
 );
 criterion_main!(benches);
