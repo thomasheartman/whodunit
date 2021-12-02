5c91e9ea60e8b1f256d773bd218c4958ab80583b
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-12-02 11:42:38 +0100

    Add more files to bench.
    Turns out the original files never had more than 997 unique elements
    for some reason.

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,7 +1,7 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use whodunit::algorithm;
 
-pub fn bench_algorithm(c: &mut Criterion) {
+pub fn bench_algorithm_max_997_unique(c: &mut Criterion) {
     let x10 = std::fs::read_to_string("./10.json").unwrap();
     let x100 = std::fs::read_to_string("./100.json").unwrap();
     let x1_000 = std::fs::read_to_string("./1000.json").unwrap();
@@ -21,20 +21,45 @@ pub fn bench_algorithm(c: &mut Criterion) {
     group.finish();
 }
 
+pub fn bench_algorithm_all_unique(c: &mut Criterion) {
+    let x10_000 = std::fs::read_to_string("./10_000-unique.json").unwrap();
+    let x100_000 = std::fs::read_to_string("./100_000-unique.json").unwrap();
+
+    let mut group = c.benchmark_group("Unique inputs");
+
+    for (i, f) in [x10_000, x100_000].iter().enumerate() {
+        let zeroes = "0".repeat(i);
+        let id = format!("10000{zeroes} elements");
+        group.bench_with_input(BenchmarkId::from_parameter(id), f, |b, input| {
+            b.iter(|| algorithm(input));
+        });
+    }
+
+    group.finish();
+}
+
 pub fn bench_algorithm_with_duplicates(c: &mut Criterion) {
     let x200_000 = std::fs::read_to_string("./200_000.json").unwrap();
+    let x200_000_two_of_each = std::fs::read_to_string("./200_000-two-of-each.json").unwrap();
 
     let mut group = c.benchmark_group("Algo range with dupes");
-    for (i, f) in [x200_000].iter().enumerate() {
-        let zeroes = "0".repeat(i + 5);
-        let id = format!("2{zeroes} elements (duplicates >= 50%)");
-        // let duplicated = f.repeat(2);
-        group.bench_with_input(BenchmarkId::from_parameter(id), f, |b, input| {
+    for (f, text) in [
+        (x200_000, "200,000 (997 unique)"),
+        (x200_000_two_of_each, "200,000 (100,000 unique)"),
+    ]
+    .iter()
+    {
+        group.bench_with_input(BenchmarkId::from_parameter(text), f, |b, input| {
             b.iter(|| algorithm(input));
         });
     }
     group.finish();
 }
 
-criterion_group!(benches, bench_algorithm, bench_algorithm_with_duplicates);
+criterion_group!(
+    benches,
+    bench_algorithm_max_997_unique,
+    bench_algorithm_with_duplicates,
+    bench_algorithm_all_unique
+);
 criterion_main!(benches);
